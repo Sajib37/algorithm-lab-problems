5412d7cfe05c9741b826a43c9951bcf0cd548e98
sajib37 <sajibhasan9811@gmail.com>
2024-02-24 20:16:21 +0600

    merge sort completed

---
+++ MergeSort.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void conquer(int arr[], int si, int mid, int ei) {
+    int merged[ei - si + 1];
+
+    int i = si;
+    int j = mid + 1;
+    int x = 0;
+    while (i <= mid && j <= ei) {
+        if (arr[i] <= arr[j]) {
+            merged[x++] = arr[i++];
+        }
+        else
+        {
+            merged[x++] = arr[j++];
+        }
+    }
+
+    while (i <= mid) {
+        merged[x++] = arr[i++];
+    }
+    while (j <= ei) {
+        merged[x++] = arr[j++];
+    }
+
+    for (int k = 0, p = si ; k < (ei - si+1);k++, p++) {
+        arr[p] = merged[k];
+    }
+}
+
+void divide(int arr[], int si, int ei) {
+    if (si >= ei)
+    {
+        return;
+    }
+    int mid = (si + (ei - si) / 2);
+    divide(arr, si, mid);
+    divide(arr, mid + 1, ei);
+
+    conquer(arr, si, mid, ei);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the length of array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << endl << "Enter your numbers: " << endl;
+    for (int i = 0;i < n;i++) {
+        cin >> arr[i];
+    }
+
+    divide(arr, 0, n - 1);
+
+
+    cout << "Sorted array is: " << endl;
+    for (int i = 0; i < n;i++) {
+        cout << arr[i] << endl;
+    }
+}
